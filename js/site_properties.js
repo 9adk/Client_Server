9db6c264cb94ba5b3e63794948b2e816b0e56ded
aditya Kharade <adityakharade51@gmail.com>
2020-11-27 01:20:34 +0530

    Added: added functionality to retrieve data from json server

--- js/site_properties.js
+++ js/site_properties.js
@@ -1,6 +1,6 @@
-let site_properties = {
-    use_local_storage = "true",
-    server_url = "http://127.0.0.1:3000/EmployeePayrollDB/" 
+const site_properties = {
+    use_local_storage : "false",
+    server_url : "http://127.0.0.1:3000/EmployeePayrollDB/" 
     // home_page: '../pages/PayrollHomePage.html',
     // add_emp_payroll_page: '../pages/EmployeePayroll.html'
 } 
